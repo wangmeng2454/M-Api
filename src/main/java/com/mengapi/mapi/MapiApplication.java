4a92cd4b1ada45591316122a680ad09dfcda8a70
WangMeng <2454238486@qq.com>
2023-07-09 12:08:50 +0800

    创建

---
+++ src/main/java/com/mengapi/mapi/MapiApplication.java
@@ -0,0 +1,13 @@
+package com.mengapi.mapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mengapi/mapi/MapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mengapi.mapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
